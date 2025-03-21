3549b80e24bcd23159f102a0b51379b42f8537df
Nikhil Arora <91010236+nikhilarora7@users.noreply.github.com>
2025-03-21 13:51:22 +0530

    Updated config.py

--- src/configuration/config.py
+++ src/configuration/config.py
@@ -1,8 +1,10 @@
+data_dir=""
 learning_rate=2e-5
 n_epochs=1
-data_dir=r"D:\entahhhtainmentttttt\testdata"
 test_split=0.1
 val_split=0.2
 workers=1
 weightpath=""
-checkpoint_savepath=r"D:\entahhhtainmentttttt\sampletestmodel.pth"
+checkpoint_savepath=""
+
+#add your own data directory,weights path and checkpoint save path.
